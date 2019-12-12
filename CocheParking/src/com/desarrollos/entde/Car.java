0262bed5647ade01335818d70163b8d2f68701ab
Jesus Martinez <jesusmartinezbeltran92@gmail.com>
2019-12-12 10:11:49 +0100

    */ Cambios realizados: minusculas necesarias cambiadas por mayusculas (String, System). Añadir static a los metodos , cambiar el objeto car por el g de garaje al llamar la funcion printGarage. Añadidos ; en el for.

--- CocheParking/src/com/desarrollos/entde/Car.java
+++ CocheParking/src/com/desarrollos/entde/Car.java
@@ -29,11 +29,11 @@ public class Car
     public static void main(String[] args) throws InterruptedException{
     	Garage g = new Garage();
     	Car car = new Car(g);
-        car.accelerate();
+        car.accelerate(); 
         slowDown();
         moveDown();
         accelerate();
-        car.printGarage();
+        g.printGarage(); //esto pertenece a g y no a car
 //        turnRight();
 //        turnHeadlightsOn();
 //        turnHeadlightsOff();
@@ -43,7 +43,7 @@ public class Car
      * Accelerate the car
      * @throws InterruptedException 
      */
-    public void accelerate() throws InterruptedException{
+    public static void accelerate() throws InterruptedException{ //metodo static
         String c = new String("*");
         String space = new String(" ");
     	for(int i=0; i<5; i++)
@@ -58,8 +58,8 @@ public class Car
      * Slow down the car
      * @throws InterruptedException 
      */
-    public void slowDown() throws InterruptedException{
-    	string c = new string("     *");
+    public static void slowDown() throws InterruptedException{  //metodo static
+    	String c = new String("     *");
     	for(int i=0; i<5; i++)
         {
         	System.out.print(c);
@@ -73,9 +73,9 @@ public class Car
      * Move the car downwards
      * @throws InterruptedException 
      */    
-    public void moveDown() throws InterruptedException{
-    	string c = new string("\t\t\t\t  *"));
-    	for(int i=0, i<5, i++)
+    public static void moveDown() throws InterruptedException{ //metodo static
+    	String c = new String("\t\t\t\t  *");  //S a mayuscula y parentesis sobrante quitado
+    	for(int i=0; i<5; i++)     //Habia una , en vez de ;
         {
     		if(i!=4)
     			System.out.println(c);
